6e3d527cd824f1b7d11eb9c42abfc026b363a5c9
Colin McNaughton <cloin@users.noreply.github.com>
2023-07-27 09:28:21 -0400

    Update toxicstack.py

--- datadog/checks.d/toxicstack.py
+++ datadog/checks.d/toxicstack.py
@@ -5,9 +5,16 @@ import time
 class ResponseTimeCheck(AgentCheck):
     def check(self, instance):
         start_time = time.time()
-        response = requests.get('http://localhost:8001/get')
+        response = requests.get('http://localhost:8001/get', stream=True)
         end_time = time.time()
 
         response_time = end_time - start_time
-
         self.gauge('toxicstack.response_time', response_time)
+
+        # Calculate connection close time
+        start_close_time = time.time()
+        response.close()
+        end_close_time = time.time()
+
+        close_time = end_close_time - start_close_time
+        self.gauge('toxicstack.close_time', close_time)
